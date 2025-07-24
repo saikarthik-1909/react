7ddc850c4846ccab7aa4107bcc025eb07be83c1b
saikarthik-1909 <saikarthiksangaru19@gmail.com>
2025-07-24 12:27:49 +0530

    Added Newsletter.css to fix build

--- frontend/src/Components/NewsLetter/Newsletter.jsx
+++ frontend/src/Components/NewsLetter/Newsletter.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import "./Newsletter.css"
+import React from "react";
+import "./NewsLetter.css";
 const Newsletter = () => {
   return (
-    <div className='newsletter'>
-        <h1>Get Exclusive Offers on Your Email</h1>
-        <p>Subscribe to our newsletter and stay updated</p>
-        <div>
-            <input type='email' placeholder='Your Email id'/>
-            <button>Subscribe</button>
-        </div>
+    <div className="newsletter">
+      <h1>Get Exclusive Offers on Your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div>
+        <input type="email" placeholder="Your Email id" />
+        <button>Subscribe</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
